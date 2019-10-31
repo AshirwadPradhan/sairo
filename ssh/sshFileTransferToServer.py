3f72f2ed7849ccd6d42e5915990c49eb17a6fe4f
suprakashmukherjee <bubai.suprakash007@gmail.com>
2019-11-01 00:43:06 +0530

    ssh code updated

--- ssh/sshFileTransferToServer.py
+++ ssh/sshFileTransferToServer.py
@@ -7,32 +7,36 @@
 
 
 import paramiko
-user_name = "shashank"
-password = "nimbus2000"
-ip = "192.168.3.3"
 
-ssh_client = paramiko.SSHClient()
-ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-print("connecting with remote server....")
-ssh_client.connect(hostname=ip,username=user_name,password=password)
+class sshFileTransfer:
+    def sshFileTransferWizard(self,user_name,password,ip):
 
-
-sftp_client = ssh_client.open_sftp()       #sftp_client object to open sftp connection with remote machine
-
-## downloading file from remote         sftp_client.get('source','destination')
-### if destination is not absolute path the downloaded file lies in the the directory of the script/cwd
-#### sftp_client.chdir("/home/shashank/")
-##### sftp_client.get('sourcefile','destinationfile')
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        print("connecting with remote server....")
+        ssh_client.connect(hostname=ip,username=user_name,password=password)
 
 
+        sftp_client = ssh_client.open_sftp()       #sftp_client object to open sftp connection with remote machine
 
-## transfer to remote
-######sftp_client.put('source','destination')
-sftp_client.put('/home/jack/Desktop/virus.txt','/home/shashank/Desktop/copiedfile.txt')
+        ## downloading file from remote         sftp_client.get('source','destination')
+        ### if destination is not absolute path the downloaded file lies in the the directory of the script/cwd
+        #### sftp_client.chdir("/home/shashank/")
+        ##### sftp_client.get('sourcefile','destinationfile')
+
+        ## transfer to remote
+        ######sftp_client.put('source','destination')
+        sftp_client.put('/home/jack/Desktop/virus.txt','/home/vm4/Desktop/copiedfile.txt')
+
+        sftp_client.close()
+        ssh_client.close()
 
 
 
 
-
-sftp_client.close()
-ssh_client.close()
+# user_name = "vm4"
+# password = "1234"
+# ip = "192.168.3.15"
+#
+# obj = sshFileTransfer()
+# obj.sshFileTransferWizard(user_name,password,ip)
