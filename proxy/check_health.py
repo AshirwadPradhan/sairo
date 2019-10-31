6ccb580fbc6fad239a8a19286822ef3be36e3cc3
suprakashmukherjee <bubai.suprakash007@gmail.com>
2019-11-01 00:10:13 +0530

    check_health.py updated

--- proxy/check_health.py
+++ proxy/check_health.py
@@ -2,7 +2,7 @@ import requests
 
 class checkHealth:
     def checkHealthMethod(self,ip):
-        res = requests.get(ip)
+        res = requests.get('http://' + ip + ':5000/')
         print(res.status_code)
         if res.status_code == 200:
             print("true")
@@ -11,6 +11,6 @@ class checkHealth:
             print("false")
             return False
 
-# ip = 'http://192.168.3.15:5000/'
-# c = checkHealth()
-# c.checkHealthMethod(ip)
+ip = '192.168.3.15'
+c = checkHealth()
+c.checkHealthMethod(ip)
